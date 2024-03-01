535df44e4f12ce23d101f67efc8e0df8badf63d8
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-01 16:21:05 -0300

    ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="app">
-      <section className='side-bar'>
-        <button>+ New Chat</button>
-        <ul className='history'></ul>
-        <nav>
+    <div className="flex h-screen w-screen">
+      <section className='h-screen w-1/6 bg-black p-5 text-white flex flex-col justify-between'>
+        <button className='border-white bg-transparent text-white w-full rounded-xl p-3 mb-10'>+ New Chat</button>
+        <ul className='flex flex-col items-center gap-5'>
+        </ul>
+        <nav className=''>
           <p>Made by João Dantas</p>
         </nav>
       </section>
 
-      <section className='main'>
-
+      <section className='bg-gray-900 h-screen w-full flex flex-col items-center p-5'>
+        <h1 className='text-3xl text-white font-bold'>CloneGPT</h1>
       </section>
     </div>
   )
