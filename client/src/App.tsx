decd047ff44f299d21a68853013ca699b4cbb305
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-02 12:51:09 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
   const [currentTitle, setCurrentTitle] = useState('');
 
   useEffect(() => {
-    console.log(currentTitle, value, message);
     if (!currentTitle && value && message) {
         setCurrentTitle(value);
     }
@@ -57,6 +56,13 @@ function App() {
     setCurrentTitle('');
   };
 
+  const handleClick = (uniqueTitle: string) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue('');
+    setCurrentTitle('');
+  };
+
   const currentChat = previousChats.filter((previousChat: any) => previousChat.title === currentTitle);
 
   const uniqueTitles = Array.from(new Set(previousChats.map((previousChat: any) => previousChat.title)));
@@ -67,7 +73,7 @@ function App() {
       <section className='h-screen w-1/6 bg-black p-5 text-white flex flex-col justify-between'>
         <button onClick={createNewChat} className='border-white bg-transparent text-white w-full rounded-xl p-3 mb-10'>+ New Chat</button>
         <ul className='flex flex-col items-center gap-5'>
-          {uniqueTitles?.map((uniqueTitle, index) => <li key={index}>{uniqueTitle}</li>)}
+          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} className="cursor-pointer" onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
         </ul>
         <nav className=''>
           <p>Made by João Dantas</p>
@@ -75,24 +81,22 @@ function App() {
       </section>
 
       <section className='bg-gray-900 h-screen w-full flex flex-col items-center justify-between p-5'>
-        {!currentTitle && <h1 className='text-3xl text-white font-bold'>CloneGPT</h1>}
+        {!currentTitle ? <h1 className='text-3xl text-white font-bold'>CloneGPT</h1> : <h1 className='text-3xl text-white font-bold'>{currentTitle}</h1>}
 
         <ul>
           {currentChat?.map((chatMessage: any, index: any) => <li key={index}>
-            <p className="font-bold">{chatMessage.role}</p>
-            <p>{chatMessage.message}</p>
+            <p className="text-gray-300 font-bold">{chatMessage.role}</p>
+            <p className="text-gray-300">{chatMessage.content}</p>
             </li>)}
         </ul>
 
         <div className='w-full'>
           <div className="flex gap-3 items-center">
-            <input value={value} onChange={(e) => setValue(e.target.value)} className='w-full h-10 bg-transparent border-2 text-white rounded border-gray-400 p-2' type='text'/>
+            <input placeholder="Digite sua mensagem" value={value} onChange={(e) => setValue(e.target.value)} className='w-full h-10 bg-transparent border-2 text-white rounded border-gray-400 p-2' type='text'/>
             <div onClick={getMessages}>
               <IoMdSend className="text-white"/>
             </div>
           </div>
-
-
 
         </div>
       </section>
