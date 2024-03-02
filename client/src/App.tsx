d929d2b4c60dc43ce6e7eada1e16ceb278c994ae
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-02 09:11:00 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
-import { useState } from 'react'
+import { IoMdSend } from "react-icons/io";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getMessages = async () => {
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        message: 'hello how are you?'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const response = await fetch('http://localhost:5000/completions', options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className="flex h-screen w-screen">
@@ -14,8 +34,24 @@ function App() {
         </nav>
       </section>
 
-      <section className='bg-gray-900 h-screen w-full flex flex-col items-center p-5'>
+      <section className='bg-gray-900 h-screen w-full flex flex-col items-center justify-between p-5'>
         <h1 className='text-3xl text-white font-bold'>CloneGPT</h1>
+
+        <ul>
+
+        </ul>
+
+        <div className='w-full'>
+          <div className="flex gap-3 items-center">
+            <input className='w-full h-10 bg-transparent border-2 text-white rounded border-gray-400 p-2' type='text'/>
+            <div onClick={getMessages}>
+              <IoMdSend className="text-white"/>
+            </div>
+          </div>
+
+
+
+        </div>
       </section>
     </div>
   )
