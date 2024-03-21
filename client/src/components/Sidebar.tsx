1b5796738added754d557600b27ab732fcab1867
João Marcelo Dantas <jmgrd98@gmail.com>
2024-03-21 00:45:49 -0300

    Update Sidebar.tsx

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useChatContext } from '../context/ChatContext';
+import { FaTrashAlt } from "react-icons/fa";
 
 const Sidebar = () => {
     const { 
@@ -19,9 +20,15 @@ const Sidebar = () => {
     };
 
     const handleClick = (uniqueTitle: string) => {
+        console.log(value)
         updateCurrentTitle(uniqueTitle);
         updateMessage(null);
         updateValue('');
+    };
+
+    const handleRemoveChat = (uniqueTitle: string) => {
+        const updatedChats = previousChats.filter((chat: any) => chat.title !== uniqueTitle);
+        updatePreviousChats(updatedChats);
     };
 
     const uniqueTitles = Array.from(new Set(previousChats.map((previousChat: any) => previousChat.title)));
@@ -30,7 +37,15 @@ const Sidebar = () => {
         <section className='h-screen w-1/6 bg-black p-5 text-white flex flex-col justify-between'>
             <button onClick={createNewChat} className='border-white bg-transparent text-white w-full rounded-xl p-3 mb-10'>+ New Chat</button>
             <ul className='flex flex-col items-center gap-5'>
-                {uniqueTitles?.map((uniqueTitle, index) => <li key={index} className="cursor-pointer bg-zinc-400/20 p-2 px-5 rounded min-w-full text-center hover:bg-zinc-400/30" onClick={() => handleClick(uniqueTitle as string)}>{uniqueTitle as string}</li>)}
+                {uniqueTitles?.map((uniqueTitle, index) => (
+                    <li 
+                        className="flex items-center justify-evenly cursor-pointer bg-zinc-400/20 p-2 px-5 rounded min-w-full text-center hover:bg-zinc-400/30"
+                        onClick={() => handleClick(uniqueTitle as string)}>
+                            <p>{uniqueTitle as string}</p>
+                            <FaTrashAlt onClick={() => handleRemoveChat(uniqueTitle as string)} className="cursor-pointer ml-2 hover:w-5 hover:h-5" />
+                    </li>
+                    )
+                )}
             </ul>
             <nav className=''>
                 <p>Made by João Dantas</p>
